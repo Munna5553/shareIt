234d4c399c2647940362a102149d73221246b703
Munna5553 <shauryakumar42040er@gmail.com>
2024-05-17 10:32:47 +0530

    project setup ✅

---
+++ src/config/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+    appwrite: String(import.meta.env.VITE_APPWRITE_URL),
+    projectId: String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    databaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    collectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    bucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+}
